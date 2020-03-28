6091c93b7c68b0c86d1f098d6fdcc49f014d8ea2
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-03-28 16:36:06 -0500

    created style provider wrapper for themes, finished nav app bar component (still needs logo pic), added node-sass to dependencies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Route } from 'react-router';
-import NaviBar from './Components/NavigationBar/NaviBar'
+import React from "react";
+import { Route } from "react-router";
+import NaviBar from "./Components/NavigationBar/NaviBar";
+import { StylesProvider } from "@material-ui/styles";
+import "./Styles/index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Route>
-        <NaviBar />
-      </Route>
-    </div>
+    <StylesProvider injectFirst>
+      <div className="App">
+        <Route>
+          <NaviBar />
+        </Route>
+      </div>
+    </StylesProvider>
   );
 }
 
--- src/Components/NavigationBar/NaviBar.js
+++ src/Components/NavigationBar/NaviBar.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import Toolbar from "@material-ui/core/Toolbar";
 // import Link from "@material-ui/core/Link";
-// import AppBar from "@material-ui/core/AppBar";
+//import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
 import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
 import { deepOrange } from "@material-ui/core/colors";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,15 +20,14 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center",
     justifyContent: "space-between",
     "& > *": {
-      margin: theme.spacing(1)
+      margin: theme.spacing(0)
     },
-    borderBottom: "solid 1px grey"
+    backgroundColor: "#44CDCD"
   },
   links: {
     textDecoration: "none",
     width: "",
-    margin: "0 0.3rem",
-    color: "#2F5973"
+    margin: "0 0.7rem"
   },
   medTitle: {
     marginLeft: 10,
@@ -35,6 +41,16 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const NavigationBar = props => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const classes = useStyles();
   const [userIsLogged, setUserIsLogged] = useState(false);
 
@@ -46,29 +62,53 @@ const NavigationBar = props => {
 
   return (
     <div className={classes.root}>
-      <div>
-        <NavLink className={classes.links} to="/">
-          <Typography className={classes.medTitle}>Flatten The Curve</Typography>
-        </NavLink>
-      </div>
-      <div>
-        {!userIsLogged ? (
-          <div>
-            <NavLink className={classes.links} to="/login">
-              Login
-            </NavLink>
-            <NavLink className={classes.links} to="/register">
-              Register
-            </NavLink>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Register</MenuItem>
+            {!userIsLogged && (
+              <MenuItem onClick={handleClose}>About us</MenuItem>
+            )}
+            <MenuItem onClick={handleClose}>Contact us</MenuItem>
+          </Menu>
+
+          <NavLink className={classes.links} to="/">
+            <Typography className={classes.medTitle}>
+              Flatten The Curve
+            </Typography>
+          </NavLink>
+
+          <div className="navLinks-div">
+            {!userIsLogged ? (
+              <div>
+                <NavLink className={classes.links} to="/login">
+                  Login
+                </NavLink>
+              </div>
+            ) : null}
           </div>
-        ) : null}
-      </div>
-      {userIsLogged ? (
-        <NavLink className={classes.links} to="/dashboard">
-          Dashboard
-        </NavLink>
-      ) : null}
-      {userIsLogged ? <Avatar className={classes.orange} /> : null}
+          {userIsLogged ? (
+            <NavLink className={classes.links} to="/dashboard">
+              Dashboard
+            </NavLink>
+          ) : null}
+          {userIsLogged ? <Avatar className={classes.orange} /> : null}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
