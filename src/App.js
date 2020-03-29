9930b7a624672649485ec9fc62888656078ce231
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-29 13:44:56 -0700

    Setting up userauth

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { Route } from "react-router";
 import NaviBar from "./Components/NavigationBar/NaviBar";
 import { StylesProvider } from "@material-ui/styles";
 import "./Styles/index.scss";
+import Registration from "./Components/Users/Registration";
+import Login from "./Components/Users/Login";
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <div className="App">
-        <Route>
-          <NaviBar />
-        </Route>
+          <Route path ='/'>
+            <NaviBar />
+          </Route>
+          <Route path ='/login'>
+            <Login />
+          </Route>
+          <Route path ='/register'>
+            <Registration />
+          </Route>
+        
       </div>
     </StylesProvider>
   );
