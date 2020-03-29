04f2db74d9468efdbf43640e1f74b8f83b5a4b71
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-29 14:44:04 -0700

    user auth and state working, need to link redux store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router";
 import NaviBar from "./Components/NavigationBar/NaviBar";
 import { StylesProvider } from "@material-ui/styles";
@@ -6,7 +6,33 @@ import "./Styles/index.scss";
 import Registration from "./Components/Users/Registration";
 import Login from "./Components/Users/Login";
 
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+
 function App() {
+
+
+  // useEffect(() => {
+  //   firebase.auth().onAuthStateChanged(function(user) {
+  //     if (user) {
+  //       // User is signed in.
+  //       var displayName = user.displayName;
+  //       var email = user.email;
+  //       var emailVerified = user.emailVerified;
+  //       var photoURL = user.photoURL;
+  //       var isAnonymous = user.isAnonymous;
+  //       var uid = user.uid;
+  //       var providerData = user.providerData;
+  //       // ...
+  //     } else {
+  //       // User is signed out.
+  //       // ...
+  //     }
+  //   });
+  // }, [])
+
   return (
     <StylesProvider injectFirst>
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+// Reducer Imports
+import userAuthReducer from './Store/Reducers/userAuthReducer'
+
+// Combined Reducers //
+const rootReducer = combineReducers({
+  auth: userAuthReducer
+})
+
+
+// Redux Store Init //
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
