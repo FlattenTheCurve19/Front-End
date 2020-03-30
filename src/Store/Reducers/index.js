1c8c718c905dd26427e51ed5ff85aa62d4fe72b4
Dylan Collins <dcollins5590@students.mc3.edu>
2020-03-30 15:09:56 -0400

    linked up the reducer inside of the combined reducer function in index.js

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { covid19Reducer } from './covid19Reducer';
+import messageBoardReducer from './messageBoardReducer';
 import userAuthReducer from './userAuthReducer';
 
 export default combineReducers({
     auth: userAuthReducer,
     covid19Reducer,
+    messageBoardReducer:  messageBoardReducer
 })
