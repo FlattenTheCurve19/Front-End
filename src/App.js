bf38f4d863bbe4b9ba1a9694d3e352ba5a0d75b7
Anthony Kawa <anthonyk2020@gmail.com>
2020-03-31 08:38:42 -0700

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -14,13 +14,8 @@ import MessageBoard from "./Components/MessageBoard";
 // Add the Firebase services that you want to use
 import * as firebase from "firebase/app";
 import "firebase/auth";
-<<<<<<< HEAD
-import Proximity from "./Components/ProximityMap/Proximity";
-import TestFeed from "./TestFeed";
-=======
 import TestFeed, { postFunc } from "./TestFeed";
 import Proximity from "./Components/ProximityMap/Proximity";
->>>>>>> 85abe3018e70a9376355a93e51b53c1f240d8c4d
 
 function App() {
   const dispatch = useDispatch();
