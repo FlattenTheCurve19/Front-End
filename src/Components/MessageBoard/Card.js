8b8a00a97346476bd33c1061c21ef6952105639c
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-01 07:48:43 -0700

    Revert "Messaging map"

--- src/Components/MessageBoard/Card.js
+++ src/Components/MessageBoard/Card.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import { Card } from './styles';
-import { useDispatch } from 'react-redux';
-import {fetchCenter} from '../../Store/Actions/messageActions';
 
 // Do a fetch to db to grab user avatar
 export default ({ message }) => {
-    const dispatch = useDispatch();
-
-    const gotToMessage = () => {
-        const center = {
-            lat: message.geoLock.latitude,
-            lng: message.geoLock.longitude
-        }
-        dispatch(fetchCenter(center))
-    }
-
     return(
-        <Card onClick={gotToMessage}>
+        <Card>
             <img src='https://images.unsplash.com/photo-1474978528675-4a50a4508dc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80'/>
             <div className='content-container'>
                 <h4>{message.displayName}</h4>
--- src/_utils/firedbHelper.js
+++ src/_utils/firedbHelper.js
@@ -1,9 +1,6 @@
 import firebase from 'firebase'
 import { fireDB } from './firebase'
 import 'firebase/firestore'
-import { GeoFirestore } from 'geofirestore';
-const geofirestore = new GeoFirestore(fireDB);
-const geocollection = geofirestore.collection('post');
 
 const post = {
   displayName: "",
@@ -16,29 +13,22 @@ const post = {
   }
 };
 
-export const createPost = (user, uuid='1354', postfield, long, lat) => {
-  // fireDB
-  //   .collection("post")
-  //   .doc()
-  //   .set({
-  //     ...post,
-  //     displayName: user,
-  //     userUUID: uuid,
-  //     postField: postfield,
-  //     geoLock: new firebase.firestore.GeoPoint(lat, long)
-  //   })
-  //   .then(() => {
-  //     console.log("Document successfully written!");
-  //   })
-  geocollection.add({
-    ...post,
-    displayName: user,
-    userUUID: uuid,
-    postField: postfield,
-    geoLock: {
-      latitude: lat,
-      longitude: long
-    },
-    coordinates: new firebase.firestore.GeoPoint(lat, long)
-  })
+export const createPost = (user, uuid, postfield, long, lat) => {
+  fireDB
+    .collection("post")
+    .doc()
+    .set({
+      ...post,
+      displayName: user,
+      userUUID: uuid,
+      postField: postfield,
+      geoLock: {
+        longitude: long,
+        latitude: lat
+      }
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+      return true;
+    })
 };
