b08abe7672fd5753a1a84097e736987b3742fae9
Josiah Roa <josiahroa18@gmail.com>
2020-04-01 12:23:03 -0600

    Added styles to add message form

--- src/Components/MessageBoard/AddMessage.js
+++ src/Components/MessageBoard/AddMessage.js
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import { messageSetter } from '../../Store/Actions/messageActions';
+import { useSelector } from 'react-redux';
 
 // Component Imports
 import { Form } from './styles';
 
+// Material UI Imports
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+}));
+
 export default ({ forceRender }) => {
-    const { handleSubmit, register, errors } = useForm();
+    const { handleSubmit, register, errors, control } = useForm({ message: '' });
     const [ user, setUser ] = useState(null);
     const history = useHistory();
+    const { userInfo } = useSelector(state => state.messageBoard);
+    const classes = useStyles();
 
     useEffect(() => {
         firebase.auth().onAuthStateChanged(user => {
@@ -23,38 +39,36 @@ export default ({ forceRender }) => {
         });
     }, [])
 
+    useEffect(() => {
+        console.log(errors);
+    }, [errors])
+
     const submitForm = (data) => {
         // Also check to see if a location has been added
-        if(user){
-            messageSetter({
-                displayName: user.displayName,
-                UUID: user.uid,
-                postField: data.message,
-                geoLock: {
-                    longitude: 0,
-                    latitude: 0
-                }
-            })
-            forceRender();
-        }else{
-            history.push('/login');
-        }
+        console.log(data);
+        // const lat = userInfo.latitude;
+        // const long = userInfo.longitude;
+        // if(!lat || !long){
+        //     console.log('Please allow location');
+        // }
+        // if(user){
+        //     messageSetter({
+        //         displayName: user.displayName,
+        //         UUID: user.uid,
+        //         postField: data.message,
+        //         geoLock: {
+        //             longitude: 0,
+        //             latitude: 0
+        //         }
+        //     })
+        //     forceRender();
+        // }else{
+        //     history.push('/login');
+        // }
     };
 
     return (
         <Form>
-            <form onSubmit={handleSubmit(submitForm)}>
-                <label>Add Message</label>
-                <input
-                    name='message'
-                    ref={register({
-                        required: true,
-                        minLength: 3,
-                        maxLength: 100
-                    })}
-                />
-                <input type='submit' value='Submit'/>
-            </form>
             {errors.message && errors.message.type === 'required' && (
                 <p>Please enter a message</p>
             )}
@@ -64,6 +78,24 @@ export default ({ forceRender }) => {
             {errors.message && errors.message.type === 'maxLength' && (
                 <p>Message cannot exceed 100 characters</p>
             )}
+            <form onSubmit={handleSubmit(submitForm)}>
+                <Controller
+                    id="standard-basic" 
+                    label="Standard" 
+                    as={<TextField/>}
+                    name='message'
+                    rules={{
+                        required: true,
+                        minLength: 3,
+                        maxLength: 100
+                    }}
+                    ref={register}
+                    control={control}
+                />
+                <div className='btn-container'>
+                    <button type='submit'>Add Message</button>
+                </div>
+            </form>
         </Form>
     )
 }
